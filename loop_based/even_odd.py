f9a41912bebe7d93b781070b764d624f8d6e41b1
Riya <riyadengree189251@gmail.com>
2026-02-01 12:43:23 +0530

    Create even_odd.py

---
+++ loop_based/even_odd.py
@@ -0,0 +1,8 @@
+# even_odd.py
+# This program checks and prints whether numbers from 1 to 10 are even or odd
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i, "is Even")
+    else:
+        print(i, "is Odd")
