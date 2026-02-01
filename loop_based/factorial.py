13ea33fc35e28402b96db8cf396586937cce027b
Riya <riyadengree189251@gmail.com>
2026-02-01 12:52:13 +0530

    Create factorial.py

---
+++ loop_based/factorial.py
@@ -0,0 +1,10 @@
+# factorial.py
+# This program finds the factorial of a number
+
+num = int(input("Enter a number: "))
+fact = 1
+
+for i in range(1, num + 1):
+    fact = fact * i
+
+print("Factorial of", num, "is:", fact)
